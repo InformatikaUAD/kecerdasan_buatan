333b407701c47bad0d8239d32b73702119f0e39b
fdhliakbar <fadhliakbar125@gmail.com>
2025-07-01 22:27:38 +0700

    feat(upload): Add simple_calculator modul in AI_PKG

---
+++ ai_pkg/simple_calculator.py
@@ -0,0 +1,14 @@
+def tambah(firstNumber, secondNumber):
+    return firstNumber + secondNumber
+
+def kurang(firstNumber, secondNumber):
+    return firstNumber - secondNumber
+
+def kali(firstNumber, secondNumber):
+    return firstNumber * secondNumber
+
+def bagi(firstNumber, secondNumber):
+    return firstNumber / secondNumber
+
+def pangkat(firstNumber, secondNumber):
+    return firstNumber ** secondNumber
--- pertemuan10.py
+++ pertemuan10.py
@@ -1,3 +1,12 @@
 import ai_pkg.lanprak as lp
+import ai_pkg.simple_calculator as scal
 
-print(lp.halo_nama("Denis Siregar"))
+# print(lp.halo_nama("Denis Siregar"))
+
+print("kalkulator Sederhana")
+print("Hasil penjumlahan 2 + 3 =", scal.tambah(2, 3))
+print("Hasil pengurangan 2 + 3 =", scal.kurang(2, 3))
+print("Hasil perkalian 2 + 3 =", scal.kali(2, 3))
+print("Hasil pembagian 2 + 3 =", scal.bagi(2, 3))
+
+print("~~~  TERIMA KASIH  ~~~")
